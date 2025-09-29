0db755d063c83ff7110352587b8c2e535c37ea41
HubertDe <dekutowski@outlook.com>
2025-09-29 23:06:07 +0200

    Push commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A Nix-flake-based C development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+     system = "x86_64-linux";
+    #system = "x86_64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+	  gcc
+	  gdb
+      ];
+  };
+};
+}
